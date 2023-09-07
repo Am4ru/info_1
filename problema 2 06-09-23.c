632dbff7db74f4136149e6eeb31aa5b08a84513a
Amaru Zegarra <101205158+Am4ru@users.noreply.github.com>
2023-09-06 21:07:56 -0300

    Add files via upload

---
+++ problema 2 06-09-23.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void incrementar_arreglo(int a[]){
+	a[1]++;
+	printf("%d\n", a[1]);
+}
+
+int main(int argc, char *argv[]) {
+
+	int temp[10] = {5,4,9};
+	printf("temp : %d\n", temp[0]);
+	incrementar_arreglo(temp);
+	printf("temp: %d\n", temp[0]);
+	printf("temp: %d\n", temp[1]);
+	
+	return 0;
+}
+
