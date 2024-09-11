c247b7e92911de4ea1002a3312d56383213b34a0
Amaru Zegarra <101205158+Am4ru@users.noreply.github.com>
2024-09-10 21:48:14 -0300

    Add files via upload

---
+++ Tp4_info1_Amaru Zegarra_91065_1R7.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	
+	int n1;
+	
+	printf("ingrese una calificacion a evaluar: ");
+	scanf("%d", &n1);
+	
+	if(n1>=90){
+		printf("calificacion A");
+	}else if (n1>=80 && n1<90){
+		printf("calificacion B");
+	}else if(n1>=70 && n1<80){
+		printf("calificacion C");
+	}else if(n1>=60 && n1<70){
+		printf("calificacion D");
+	}else if(n1<60){
+		printf("calificacion F");
+	}
+	
+	return 0;
+}
+
